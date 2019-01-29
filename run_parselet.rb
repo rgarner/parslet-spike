ff24047fbc4c074991cd7bf8f3912d12a1372ac7
Russell Garner <rgarner@zephyros-systems.co.uk>
2019-01-29 08:19:38 +0000

    A fuller example that:
    - Has a full metadata block at the start
    - Captures management percent or 'custom'
    - Transforms the hash to a simpler parseable one

--- run_parselet.rb
+++ run_parselet.rb
@@ -1,35 +1,43 @@
 require 'parslet'
+require 'bigdecimal'
 
 class FrameworkDefinitionParser < Parslet::Parser
   root(:framework)
   rule(:framework) do
-    str('Framework') >> space? >> framework_identifier.as(:framework_short_name) >>
+    str('Framework') >> space? >> framework_identifier.as(:framework_short_name) >> space? >>
       framework_block >>
     space?
   end
 
   rule(:framework_block) do
-    lbrace >> name >> space? >> invoice_fields.maybe.as(:invoice_fields) >> rbrace
+    lbrace >> metadata >> space? >> invoice_fields.maybe.as(:invoice_fields) >> rbrace
   end
 
-  rule(:framework_identifier)   { match(/[A-Z0-9\/]/).repeat(1) >> space? }
-  rule(:snake_case_identifier)  { match(/[a-z0-9_]/).repeat(1) >> space? }
-  rule(:pascal_case_identifier) { (match(/[A-Z]/) >> match(/[a-z]/).repeat).repeat >> space? }
+  rule(:framework_identifier)   { match(/[A-Z0-9\/]/).repeat(1) }
+  rule(:snake_case_identifier)  { match(/[a-z0-9_]/).repeat(1) }
+  rule(:pascal_case_identifier) { (match(/[A-Z]/) >> match(/[a-z]/).repeat).repeat }
 
+  rule(:metadata)               { name >> management_charge }
   rule(:name)                   { str('name') >> space? >> string.as(:name) >> space? }
+  rule(:management_charge) do
+    str('management_charge_rate') >> space? >> (percentage | str('custom')).as(:management_charge_rate) >> space?
+  end
+  rule(:percentage)             { integer.as(:percentage) >> str('%') >> space? }
 
-  rule(:invoice_fields)         { str('Invoices') >> space? >> field_block }
+  rule(:invoice_fields)         { str('Invoice') >> space? >> field_block }
   rule(:field_block)            { lbrace >> total_value_field >> field_defs.maybe >> rbrace >> space? }
   rule(:total_value_field)      { str('total_value_field') >> space? >> string.as(:total_value_field) >> space? }
-  rule(:field_defs)             { field_def.repeat(1) >> space? }
-  rule(:field_def)              { str('field') >> space? >> pascal_case_identifier.as(:field_name) >> field_options.maybe >> space? }
-  rule(:field_options)          { str(',') >> space? >> str('from:') >> space? >> string.as(:source_field) }
+  rule(:field_defs)             { field_def.repeat }
+  rule(:field_def)              { str('field') >> space? >> pascal_case_identifier.as(:field) >> field_options.maybe >> space? }
+  rule(:field_options)          { str(',') >> space? >> str('from:') >> space? >> string.as(:from) }
 
   rule(:string) {
     str("'") >> (
       str("'").absent? >> any
     ).repeat.as(:string) >> str("'")
   }
+
+  rule(:integer) { match(/[0-9]/).repeat }
 
   rule(:space)  { match('\s').repeat(1) }
   rule(:space?) { space.maybe }
@@ -41,9 +49,10 @@ end
 doc = <<~EOF
   Framework CM/OSG/05/3565 {
     name 'My framework name'
+    management_charge_rate 1%
 
-    Invoices {
-      total_value_field 'Hi'
+    Invoice {
+      total_value_field 'Total Cost or Something'
 
       field FrankZappa
       field SarahConnor, from: 'The Terminator'
@@ -59,3 +68,13 @@ begin
 rescue Parslet::ParseFailed => e
   puts e.parse_failure_cause.ascii_tree
 end
+
+puts '*******'
+
+class SomeTransform < Parslet::Transform
+  rule(:string => simple(:s))      { String(s) }
+  rule(:percentage => simple(:i) ) { BigDecimal(i) }
+end
+
+t = SomeTransform.new
+pp t.apply(slice)
