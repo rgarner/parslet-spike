e25cd0a65efc9d90c5b78efbfa51915895439c35
Russell Garner <rgarner@zephyros-systems.co.uk>
2019-01-31 15:59:15 +0000

    Add indicative parser specs for field_def
    Not all contexts are covered.

--- spec/framework/definition/parser_spec.rb
+++ spec/framework/definition/parser_spec.rb
@@ -20,5 +20,25 @@ describe Framework::Definition::Parser do
         it { is_expected.to parse('RM3786') }
       end
     end
+
+    describe '#field_def' do
+      subject { parser.field_def }
+
+      context 'A known field' do
+        it { is_expected.to parse("TotalValue from 'Total Spend'") }
+      end
+
+      context 'An additional field' do
+        it { is_expected.to parse("Date Additional8 from 'A lovely date'") }
+
+        context 'that is optional' do
+          it { is_expected.to parse("Date Additional8 from 'A lovely date' optional") }
+        end
+      end
+
+      context 'an unknown field' do
+        it { is_expected.to parse("Decimal 'Price of fish'") }
+      end
+    end
   end
 end
