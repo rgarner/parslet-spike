037b8b41ebea260b51b806c5a01aa0f2a19f1458
Russell Garner <rgarner@zephyros-systems.co.uk>
2019-01-31 08:19:53 +0000

    Move spike parser to Framework::Definition::Parser

---
+++ lib/framework/definition/parser.rb
@@ -0,0 +1,71 @@
+module Framework
+  module Definition
+    class Parser < Parslet::Parser
+      module Type
+        INTEGER = 'Integer'.freeze
+        STRING  = 'String'.freeze
+        DECIMAL = 'Decimal'.freeze
+        DATE    = 'Date'.freeze
+        BOOLEAN = 'Boolean'.freeze
+      end
+
+      root(:framework)
+      rule(:framework) do
+        str('Framework') >>
+          spaced(framework_identifier.as(:framework_short_name)) >>
+          spaced(framework_block)
+      end
+
+      rule(:framework_block) do
+        braced(
+          spaced(metadata) >>
+            spaced(fields_blocks.maybe.as(:entry_data))
+        )
+      end
+
+      rule(:framework_identifier)   { match(%r{[A-Z0-9/]}).repeat(1) }
+      rule(:pascal_case_identifier) { (match(/[A-Z]/) >> match(/[a-z]/).repeat).repeat(1) }
+
+      rule(:metadata)               { name >> management_charge }
+      rule(:name)                   { str('Name') >> spaced(string.as(:name)) }
+      rule(:management_charge)      { (str('ManagementChargeRate') >> spaced(percentage)).as(:management_charge_rate) }
+      rule(:percentage)             { float.as(:percentage) >> str('%') }
+
+      rule(:fields_blocks)          { (invoice_fields >> contract_fields.maybe) | (contract_fields >> invoice_fields.maybe) }
+      rule(:invoice_fields)         { (str('InvoiceFields') >> spaced(field_block)).as(:invoice_fields) }
+      rule(:contract_fields)        { (str('ContractFields') >> spaced(field_block)).as(:contract_fields) }
+      rule(:field_block)            { braced(field_defs) }
+      rule(:field_defs)             { field_def.repeat(1) }
+      rule(:field_def)              { known_field | additional_field | unknown_field }
+      rule(:known_field)            { pascal_case_identifier.as(:field) >> field_source }
+      rule(:additional_field)       { spaced(typedef).as(:type) >> (str('Additional') >> match(/[1-8]/)).as(:field) >> field_source }
+      rule(:unknown_field)          { spaced(typedef).as(:type) >> field_source }
+      rule(:field_source)           { spaced(str('from')).maybe >> string.as(:from) >> spaced(optional.as(:optional).maybe) }
+      rule(:typedef)                { str(Type::INTEGER) | str(Type::STRING) | str(Type::DECIMAL) | str(Type::DATE) | str(Type::BOOLEAN) }
+      rule(:optional)               { str('optional') }
+
+      rule(:string) {
+        str("'") >> (
+          str("'").absent? >> any
+        ).repeat.as(:string) >> str("'")
+      }
+
+      rule(:integer) { match(/[0-9]/).repeat }
+      rule(:float)   { integer >> (str('.') >> match('[0-9]').repeat(1)).as(:float) >> space? }
+
+      rule(:space)  { match('\s').repeat(1) }
+      rule(:space?) { space.maybe }
+
+      rule(:lbrace) { str('{') >> space? }
+      rule(:rbrace) { str('}') >> space? }
+
+      def spaced(atom)
+        space? >> atom >> space?
+      end
+
+      def braced(atom)
+        lbrace >> atom >> rbrace
+      end
+    end
+  end
+end
--- run_parselet.rb
+++ run_parselet.rb
@@ -1,73 +1,8 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'lib'))
+
 require 'parslet'
 require 'bigdecimal'
-
-class FrameworkDefinitionParser < Parslet::Parser
-  module Type
-    INTEGER = 'Integer'.freeze
-    STRING  = 'String'.freeze
-    DECIMAL = 'Decimal'.freeze
-    DATE    = 'Date'.freeze
-    BOOLEAN = 'Boolean'.freeze
-  end
-
-  root(:framework)
-  rule(:framework) do
-    str('Framework') >>
-      spaced(framework_identifier.as(:framework_short_name)) >>
-      spaced(framework_block)
-  end
-
-  rule(:framework_block) do
-    braced(
-      spaced(metadata) >>
-      spaced(fields_blocks.maybe.as(:entry_data))
-    )
-  end
-
-  rule(:framework_identifier)   { match(%r{[A-Z0-9/]}).repeat(1) }
-  rule(:pascal_case_identifier) { (match(/[A-Z]/) >> match(/[a-z]/).repeat).repeat(1) }
-
-  rule(:metadata)               { name >> management_charge }
-  rule(:name)                   { str('Name') >> spaced(string.as(:name)) }
-  rule(:management_charge)      { (str('ManagementChargeRate') >> spaced(percentage)).as(:management_charge_rate) }
-  rule(:percentage)             { float.as(:percentage) >> str('%') }
-
-  rule(:fields_blocks)          { (invoice_fields >> contract_fields.maybe) | (contract_fields >> invoice_fields.maybe) }
-  rule(:invoice_fields)         { (str('InvoiceFields') >> spaced(field_block)).as(:invoice_fields) }
-  rule(:contract_fields)        { (str('ContractFields') >> spaced(field_block)).as(:contract_fields) }
-  rule(:field_block)            { braced(field_defs) }
-  rule(:field_defs)             { field_def.repeat(1) }
-  rule(:field_def)              { known_field | additional_field | unknown_field }
-  rule(:known_field)            { pascal_case_identifier.as(:field) >> field_source }
-  rule(:additional_field)       { spaced(typedef).as(:type) >> (str('Additional') >> match(/[1-8]/)).as(:field) >> field_source }
-  rule(:unknown_field)          { spaced(typedef).as(:type) >> field_source }
-  rule(:field_source)           { spaced(str('from')).maybe >> string.as(:from) >> spaced(optional.as(:optional).maybe) }
-  rule(:typedef)                { str(Type::INTEGER) | str(Type::STRING) | str(Type::DECIMAL) | str(Type::DATE) | str(Type::BOOLEAN) }
-  rule(:optional)               { str('optional') }
-
-  rule(:string) {
-    str("'") >> (
-      str("'").absent? >> any
-    ).repeat.as(:string) >> str("'")
-  }
-
-  rule(:integer) { match(/[0-9]/).repeat }
-  rule(:float)   { integer >> (str('.') >> match('[0-9]').repeat(1)).as(:float) >> space? }
-
-  rule(:space)  { match('\s').repeat(1) }
-  rule(:space?) { space.maybe }
-
-  rule(:lbrace) { str('{') >> space? }
-  rule(:rbrace) { str('}') >> space? }
-
-  def spaced(atom)
-    space? >> atom >> space?
-  end
-
-  def braced(atom)
-    lbrace >> atom >> rbrace
-  end
-end
+require 'framework/definition/parser'
 
 # InvoiceFields specified in PascalCase.
 # Each field is a known destination with a known type, so type annotations are
@@ -101,10 +36,10 @@ doc = <<~EOF
   }
 EOF
 
-FrameworkDefinitionParser.new.pascal_case_identifier.parse('FrankZappa')
+Framework::Definition::Parser.new.pascal_case_identifier.parse('FrankZappa')
 
 begin
-  slice = FrameworkDefinitionParser.new.parse(doc, reporter: Parslet::ErrorReporter::Deepest.new)
+  slice = Framework::Definition::Parser.new.parse(doc, reporter: Parslet::ErrorReporter::Deepest.new)
   pp slice
 rescue Parslet::ParseFailed => e
   puts e.parse_failure_cause.ascii_tree
